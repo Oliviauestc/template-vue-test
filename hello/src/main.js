afe5527cda10d1109e613703f2d80b774b52aa90
管理员 <admin@feis-iMac.local>
2020-10-12 17:35:06 +0800

    Feat：升级到vue-cli 3.0之后的模板，选择是否添加router

--- hello/src/main.js
+++ hello/src/main.js
@@ -1,20 +1,14 @@
-{{#if_eq build "standalone"}}
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-{{/if_eq}}
 import Vue from 'vue'
 import App from './App'
-{{#router}}
 import router from './router'
-{{/router}}
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  {{#router}}
   router,
-  {{/router}}
   render: h => h(App)
 }).$mount('#app')
